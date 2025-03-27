279c2d7bd3790fdf083887fa5e32c00ff5eced09
harry <harry.harry.rosadi@gmail.com>
2025-03-27 12:55:07 +0700

    buku biasa unit tes

--- src/main/java/com/demo/tes/backend/exception/security/ServiceException.java
+++ src/main/java/com/demo/tes/backend/exception/security/ServiceException.java
@@ -1,6 +1,9 @@
 package com.demo.tes.backend.exception.security;
 
+import java.io.Serial;
+
 public class ServiceException extends Exception {
+    @Serial
     private static final long serialVersionUID = 8024895122045859786L;
 
     public ServiceException(String message) {
